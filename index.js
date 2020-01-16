e99e2ea5d900649c7097354dbf00de9d42117ae0
eko <emasovic>
2020-01-16 16:44:26 +0100

    Updates

--- index.js
+++
@@ -1,8 +0,0 @@
-import './index.scss'
-import ReactDOM from 'react-dom'
-import Routes from './routes'
-
-ReactDOM.render(
-  Routes,
-  document.getElementById('root')
-)
---
+++ precache-manifest.e520b0e558f3e6963794b70f7e8d28ff.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dc9e063c630e7b899d113c25ccefc69",
+    "url": "/memorygame/index.html"
+  },
+  {
+    "revision": "2f775e9681ebc542189d",
+    "url": "/memorygame/static/css/main.1b60560e.chunk.css"
+  },
+  {
+    "revision": "9b4cfe9b9182cee9e0ef",
+    "url": "/memorygame/static/js/2.2ba059f4.chunk.js"
+  },
+  {
+    "revision": "be3eb4cbad5a344e9603583a731df32d",
+    "url": "/memorygame/static/js/2.2ba059f4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2f775e9681ebc542189d",
+    "url": "/memorygame/static/js/main.1bc7de7b.chunk.js"
+  },
+  {
+    "revision": "c370dbd133ac82173471",
+    "url": "/memorygame/static/js/runtime-main.117d0813.js"
+  },
+  {
+    "revision": "2136392735279f8fd3fafa7b0e6cca5c",
+    "url": "/memorygame/static/media/ana.21363927.mp3"
+  },
+  {
+    "revision": "539d0b2fd37d9a1a28b5f94a17f81e65",
+    "url": "/memorygame/static/media/ana.539d0b2f.gif"
+  },
+  {
+    "revision": "e5fbc83652f2661f3b7b70afbc6819a9",
+    "url": "/memorygame/static/media/ana.e5fbc836.png"
+  },
+  {
+    "revision": "584e4a3267159e4aa9664ff8db0526c0",
+    "url": "/memorygame/static/media/dunja.584e4a32.png"
+  },
+  {
+    "revision": "5e57f1db179f2466bb05229345c28aa9",
+    "url": "/memorygame/static/media/dunja.5e57f1db.gif"
+  },
+  {
+    "revision": "7f546bc5214fe3373657bb78640b5929",
+    "url": "/memorygame/static/media/dunja.7f546bc5.mp3"
+  },
+  {
+    "revision": "52b5951cd0347531c1e4a05fe21c3e90",
+    "url": "/memorygame/static/media/dusica.52b5951c.mp3"
+  },
+  {
+    "revision": "ba05ba9d91132a02a119b01f1564fde2",
+    "url": "/memorygame/static/media/dusica.ba05ba9d.png"
+  },
+  {
+    "revision": "f977a5a1b2e513e34a8ce89e4c84c5f4",
+    "url": "/memorygame/static/media/dusica.f977a5a1.gif"
+  },
+  {
+    "revision": "71dc6b94ac8e2cbb605e0f5d26c9c4f1",
+    "url": "/memorygame/static/media/jasmina.71dc6b94.mp3"
+  },
+  {
+    "revision": "7d64192dc83412ba0fcf87f2bcdf1347",
+    "url": "/memorygame/static/media/jasmina.7d64192d.png"
+  },
+  {
+    "revision": "85e57d0c65e5de6774695a1957e12db4",
+    "url": "/memorygame/static/media/jasmina.85e57d0c.gif"
+  },
+  {
+    "revision": "7ff5091d7ae6db6bd0ac9935b8e3af3f",
+    "url": "/memorygame/static/media/mare.7ff5091d.png"
+  },
+  {
+    "revision": "b125e09978071b891919e8044c0ddd41",
+    "url": "/memorygame/static/media/mare.b125e099.gif"
+  },
+  {
+    "revision": "d3a322e5b6d5667e5dcad17003ba11a8",
+    "url": "/memorygame/static/media/mare.d3a322e5.mp3"
+  },
+  {
+    "revision": "943434401ceb46e130ec1f1b02ed4317",
+    "url": "/memorygame/static/media/mi.94343440.gif"
+  },
+  {
+    "revision": "d2e5b10ea59da9c59277dd9871f91cc2",
+    "url": "/memorygame/static/media/mi.d2e5b10e.mp3"
+  },
+  {
+    "revision": "100aa9b5496a3fa1d90e1fc2ee27aad8",
+    "url": "/memorygame/static/media/milos.100aa9b5.png"
+  },
+  {
+    "revision": "3eca90d6bbf87a086d7b0ef590ec168e",
+    "url": "/memorygame/static/media/milos.3eca90d6.gif"
+  },
+  {
+    "revision": "bf0ad00e614b495d9d506aead68157a6",
+    "url": "/memorygame/static/media/milos.bf0ad00e.mp3"
+  },
+  {
+    "revision": "485a18aec4151dfb1ff1080c3af86414",
+    "url": "/memorygame/static/media/nela.485a18ae.mp3"
+  },
+  {
+    "revision": "b99df0224ac2cd6fce37598028d5a94b",
+    "url": "/memorygame/static/media/nela.b99df022.gif"
+  },
+  {
+    "revision": "e20a53435676f80d105f2f7aa0d8e21f",
+    "url": "/memorygame/static/media/nela.e20a5343.png"
+  },
+  {
+    "revision": "44bc7e7a18ba7c2fba93ab22a3f39288",
+    "url": "/memorygame/static/media/tamara.44bc7e7a.mp3"
+  },
+  {
+    "revision": "c4dc03a0230e8e37e3f1cea6f395398a",
+    "url": "/memorygame/static/media/tamara.c4dc03a0.png"
+  },
+  {
+    "revision": "e34a822b620f850cc1e81a32c2e8c4cb",
+    "url": "/memorygame/static/media/tamara.e34a822b.gif"
+  },
+  {
+    "revision": "2025ecfed522244f2dd97369c27cbe6a",
+    "url": "/memorygame/static/media/teodora.2025ecfe.png"
+  },
+  {
+    "revision": "2ff54cd8e7529ab15a89fd2af02fb29b",
+    "url": "/memorygame/static/media/teodora.2ff54cd8.gif"
+  },
+  {
+    "revision": "afd360d2c7ce0be4843389a398518bbb",
+    "url": "/memorygame/static/media/teodora.afd360d2.mp3"
+  },
+  {
+    "revision": "60e1b3e1673b5bf03e7e644e48753983",
+    "url": "/memorygame/static/media/veljko.60e1b3e1.mp3"
+  },
+  {
+    "revision": "89430aeb535cfaad725bc4a0e2f722d9",
+    "url": "/memorygame/static/media/veljko.89430aeb.gif"
+  },
+  {
+    "revision": "af6037575808b3bd57a51c69844e642f",
+    "url": "/memorygame/static/media/veljko.af603757.png"
+  }
+]);
